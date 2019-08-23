35fa5a4e0abc373c785e4fa582fe91cb25c2cf82
Mohammed Hayat <mohammedjhayat@gmail.com>
2019-08-22 17:13:09 -0700

    UI refactor

--- test.py
+++ test.py
@@ -1,26 +1,130 @@
 import sys
+import json
+import hashlib
 
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
-class MainWindow(QtWidgets.QMainWindow):
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class MainWindow(QMainWindow):
+
+    # fill in dimension and general window variables
     def __init__(self):
-        super(MainWindow, self).__init__()
-        uic.loadUi('test.ui', self)
-	self.onButton.clicked.connect(self.turnOn)
-	self.offButton.clicked.connect(self.turnOff)
-	self.intensitySlider.valueChanged.connect(self.value)
+        super().__init__()
+        self.title = 'IEEE Espresso Machine'
+        self.left = 500
+        self.top = 500
+        self.width = 500
+        self.height = 250
+        self.start_ui()
 
-    def turnOn(self):
-	print("hello")
 
-    def turnOff(self):
-	print("off")
+    # place window variables in UI
+    def start_ui(self):
+        # set window title
+        self.setWindowTitle(self.title)
 
-    def value(self):
-	print(self.intensitySlider.value())
+        # set window dimensions
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        # initialize LMU Logo
+        self.logo = QLabel(self)
+        self.pixmap = QPixmap('image.png')
+        self.logo.setPixmap(self.pixmap)
+        self.logo.resize(self.pixmap.width(), self.pixmap.height())
+        self.logo.move(212, 10)
+
+        # initialize returning user label
+        self.returning_user_label = QLabel(self)
+        self.returning_user_label.setText('Returning Users')
+        self.returning_user_label.move(50, 50)
+
+        # initialize text field for returning user
+        self.text_input_returning_user = QLineEdit(self)
+        self.text_input_returning_user.resize(100, 100)
+        self.text_input_returning_user.move(50, 100)
+
+
+        # initialize brewing button, connect to turnOn method
+        self.brew_button = QPushButton('Start Brewing!', self)
+        self.brew_button.resize(150, 32)
+        self.brew_button.move(27, 215)
+        self.brew_button.clicked.connect(self.begin_brew)
+
+        # initialize new user label
+        self.new_user_label = QLabel(self)
+        self.new_user_label.setText('New Users')
+        self.new_user_label.move(350, 50)
+
+        # initialize text field for returning user
+        self.text_input_new_user = QLineEdit(self)
+        self.text_input_new_user.resize(100, 100)
+        self.text_input_new_user.move(350, 100)
+
+        # initialize brewing button, connect to turnOn method
+        self.creation_button = QPushButton('Create New User', self)
+        self.creation_button.resize(150, 32)
+        self.creation_button.move(327, 215)
+        self.creation_button.clicked.connect(self.create_user)
+
+    # begin brew for returning users
+    @pyqtSlot()
+    def begin_brew(self):
+        drinks = ('Espresso', 'Cappuccino', 'Latte')
+        search_key = self.encode_card(self.text_input_returning_user.text())
+
+        with open('user_db.txt', 'r') as f:
+            users = f.read()
+        users_json = json.loads(users) if users else []
+
+        contact = None
+        for user in users_json:
+            if user['card'] == search_key:
+                contact = user
+                break
+
+        if contact == None:
+            QMessageBox.critical(self, 'Error!', 'User Not Found, trying creating a new profile.', QMessageBox.Ok, QMessageBox.Ok)
+        else:
+            contact_name = contact['name']
+            contact_coffee = drinks[contact['coffee']-1]
+            QMessageBox.information(self, 'Hello, {}!'.format(contact_name.split(' ')[0]), 'Hello {name}!\nOne {choice}, coming right up!'.format(name=contact_name,choice=contact_coffee))
+            # TODO: THIS IS WHERE WE SEND THE SIGNAL TO THE RELAY
+        self.text_input_returning_user.setText('')
+
+
+    # create new user profile
+    def create_user(self):
+        name = QInputDialog.getText(self, 'Get Name', 'Enter Your Name:', QLineEdit.Normal, '')[0]
+        drinks = ('Espresso', 'Cappuccino', 'Latte')
+        drink_choice, ok_pressed = QInputDialog.getItem(self, "Get Drink", "Select Coffee Type:", drinks, 0, False)
+        if drink_choice and ok_pressed:
+            choice_in_integer = drinks.index(drink_choice) + 1
+            self.save_user(name, choice_in_integer, self.encode_card(self.text_input_new_user.text()))
+            self.text_input_new_user.setText('')
+            QMessageBox.information(self, 'Success!', 'User Created!', QMessageBox.Ok,
+                                 QMessageBox.Ok)
+
+    # save new user profile to list of existing users
+    def save_user(self, name, choice, card):
+        json_profile = {'card':card, 'name':name,'coffee':choice}
+        with open('user_db.txt', 'r') as f:
+            users = f.read()
+
+        with open('user_db.txt', 'w') as f:
+            users_json = json.loads(users) if users else []
+            users_json.append(json_profile)
+            f.write(json.dumps(users_json))
+
+    # hash card to store safely in users_db
+    def encode_card(self, card):
+        hash = hashlib.sha256(card.encode())
+        return hash.hexdigest()
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
     sys.exit(app.exec_())
